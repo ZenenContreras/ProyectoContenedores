790d78a5b7727d3ce9957258ae933eead1407f5f
Zenen Contreras <zenencontreras1@gmail.com>
2026-03-19 09:21:14 -0500

    feat: configurar el backend con Express y agregar rutas de microservicios

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const microservicesRoutes = require('./routes/microservices.routes')
+const PORT = 5500
 
-app.listen(5500, () => {
-    console.log('Backend Microservicios corriendo en http://localhost:5500')
+// Permitir peticiones desde el front
+app.use(cors())
+
+// Entender los json de req desde el frontend
+app.use(express.json())
+
+//Ruta de microservicios
+app.use('/api/microservices', microservicesRoutes)
+
+app.get('/' , (req, res) => {
+    res.json({message: 'Backend de microservicios funcionando correctamente'})
+})
+
+app.listen(PORT, () => {
+    console.log(`Backend Microservicios corriendo en http://localhost:${PORT}`)
 })
 
